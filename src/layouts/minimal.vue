57e8c4380884855bb85396a0c135c6b4d976d45e
Jereme Allen <jereme.allen@wheniwork.com>
2017-08-16 18:01:34 -0500

    Add login page

---
+++ src/layouts/minimal.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <v-grid variant='container'>
+      <v-col :variants="['md-6', 'md-offset-3']">
+        <slot></slot>
+      </v-col>
+    </v-grid>
+  </div>
+</template>
+<script>
+  export default {
+    name: 'minimal-layout',
+  };
+</script>
--- src/pages/login/index.vue
+++ src/pages/login/index.vue
@@ -1,14 +1,60 @@
 <template>
-  <div>
-      {{ pageName }}
-  </div>
+  <v-layout>
+    <v-grid variant='container'>
+      <v-row>
+        <v-col>
+          <v-card>
+            <v-card-heading>
+              <v-card-title>Login</v-card-title>
+            </v-card-heading>
+            <v-card-body>
+              <v-form @submit.prevent.native="login">
+                <v-form-group>
+                    <v-text-field 
+                      placeholder="Email"
+                      variant="block"
+                      v-model="user.username"
+                      type="email"
+                    />
+                </v-form-group>
+                <v-form-group>
+                  <v-text-field 
+                      placeholder="Password"
+                      variant="block"
+                      v-model="user.password"
+                      type="password"
+                    />
+                </v-form-group>
+                <v-form-group>
+                  <v-button 
+                      @click.native="login"
+                      variant="block"
+                  >Login</v-button>
+                </v-form-group>
+              </v-form>
+            </v-card-body>
+          </v-card>
+        </v-col>
+      </v-row>
+    </v-grid>
+  </v-layout>
 </template>
 <script>
   export default {
+    name: 'login-index',
     data() {
       return {
-        pageName: 'login.index',
+        user: {
+          username: '',
+          password: '',
+        },
       };
+    },
+    methods: {
+      login() {},
+    },
+    components: {
+      VLayout: require('@/layouts/minimal'),
     },
   };
 </script>
