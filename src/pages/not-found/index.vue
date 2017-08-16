ceec5d5a40ce35bd1bd3e53f7fc3185e5fb64abf
Jereme Allen <jereme.allen@wheniwork.com>
2017-08-16 17:51:03 -0500

    Not found page

--- src/pages/not-found/index.vue
+++ src/pages/not-found/index.vue
@@ -1,14 +1,24 @@
 <template>
-  <div>
-      {{ pageName }}
-  </div>
+  <v-layout>
+    <v-grid variant='container'>
+      <v-row>
+        <v-col>
+          <v-card>
+            <v-card-heading>
+              <v-card-title>404</v-card-title>
+            </v-card-heading>
+            <v-card-body>This page does not exist ...</v-card-body>
+          </v-card>
+        </v-col>
+      </v-row>
+    </v-grid>
+  </v-layout>
 </template>
 <script>
   export default {
-    data() {
-      return {
-        pageName: 'not-found.index',
-      };
+    name: 'not-found-index',
+    components: {
+      VLayout: require('@/layouts/base'),
     },
   };
 </script>
